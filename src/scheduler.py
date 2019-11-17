d13da7bc19822fcf6bde274cadd7a68ce565025b
Chip Newman <chip594@gmail.com>
2019-11-16 20:46:57 -0600

    changed variable names to help readability

--- src/scheduler.py
+++ src/scheduler.py
@@ -9,79 +9,79 @@
 
 import sys
 
-def run_me_daily(hour, minute, command):
-    print_job(hour, minute, determine_day(hour, minute), command)
+def run_me_daily(config_hour, config_minute, command):
+    print_job(config_hour, config_minute, determine_day(config_hour, config_minute), command)
 
-def run_me_hourly(hour, minute, command):
+def run_me_hourly(config_hour, config_minute, command):
     # Determines whether to roll over to the next hour by comparing
     # the current minute to the scheduler configuration input minute.
-    if (current_minute > minute):
-        hour += 1
+    if (current_minute > config_minute):
+        config_hour += 1
 
         # Rolls over to the next day if the increase causes
         # the hour to exceed 23 hours.
-        if (hour > 23):
-            hour = hour - 24
+        if (config_hour > 23):
+            config_hour = config_hour - 24
 
-    print_job(hour, minute, determine_day(hour, minute), command)
+    print_job(config_hour, config_minute, determine_day(config_hour, config_minute), command)
 
-def run_me_every_minute(hour, minute, command):
-    print_job(hour, minute, determine_day(hour, minute), command)
+def run_me_every_minute(config_hour, config_minute, command):
+    print_job(config_hour, config_minute, determine_day(config_hour, config_minute), command)
 
 # Number of executions will be limited by the counter.
-def run_me_sixty_times(hour, minute, command):
+def run_me_sixty_times(config_hour, config_minute, command):
     global counter
 
     # Starts at the top of the hour if current hour and 
     # scheduled configuration input hour aren't the same.
     # (current minute == minutes) returns true if the scheduled
     # configuration input minute value is "*".
-    if (current_hour != hour) and (current_minute == minute):
-        minute = 0
+    if (current_hour != config_hour) and (current_minute == config_minute):
+        config_minute = 0
 
     if (counter < 60):
-        print_job(hour, minute, determine_day(hour, minute), command)
+        print_job(config_hour, config_minute, determine_day(config_hour, config_minute), command)
         counter += 1
 
 # Determines what day to display by comparing the current time
 # passed in and the scheduled configuration input time.
-def determine_day(hour, minute):
-    if (current_hour > hour):
+def determine_day(config_hour, config_minute):
+    if (current_hour > config_hour):
         return 'tomorrow'
 
-    if (current_hour == hour) and (current_minute > minute):
+    if (current_hour == config_hour) and (current_minute > config_minute):
         return 'tomorrow'
     
     return 'today'
 
-def print_job(hour, minute, day, command):
-    sys.stdout.write('{}:{:02} {} - {}\n'.format(hour, minute, day, command))
+def print_job(config_hour, config_minute, day, command):
+    sys.stdout.write('{}:{:02} {} - {}\n'.format(config_hour, config_minute, day, command))
 
 def main():
     for line in sys.stdin:
-        minute, hour, command = line.split()
+        config_minute, config_hour, command = line.split()
 
-        if (minute == '*'):
-            minute = current_minute
+        if (config_minute == '*'):
+            config_minute = current_minute
         else:
-            minute = int(minute)
+            config_minute = int(config_minute)
 
-        if (hour == '*'):
-            hour = current_hour
+        if (config_hour == '*'):
+            config_hour = current_hour
         else:
-            hour = int(hour)
+            config_hour = int(config_hour)
 
         if (command == '/bin/run_me_daily'):
-            run_me_daily(hour, minute, command)
+            run_me_daily(config_hour, config_minute, command)
 
         if (command == '/bin/run_me_hourly'):
-            run_me_hourly(hour, minute, command)
+            run_me_hourly(config_hour, config_minute, command)
 
         if (command == '/bin/run_me_every_minute'):
-            run_me_every_minute(hour, minute, command)
+            run_me_every_minute(config_hour, config_minute, command)
 
         if (command == '/bin/run_me_sixty_times'):
-            run_me_sixty_times(hour, minute, command)
+            run_me_sixty_times(config_hour, config_minute, command)
 
 if __name__ == '__main__':
     current_time = sys.argv[1]
